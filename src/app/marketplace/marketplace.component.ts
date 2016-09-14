3b4c26da587d8aec3a645e3e3874705f3d90d4cb
michael.falkenthal@iaas.uni-stuttgart.de <michael.falkenthal@iaas.uni-stuttgart.de>
2016-09-14 12:41:52 +0200

    add category.model and update marketplace component

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,7 +1,9 @@
 import {Component, Output, EventEmitter, OnInit} from '@angular/core';
 import {ApplicationService} from "../shared/application.service";
 import {Application} from "../shared/application.model";
-import {Subject, Observable} from "rxjs";
+import {Category} from "../shared/category.model";
+import {Observable} from "rxjs/Observable";
+import {Subject} from "rxjs/Subject";
 
 @Component({
     selector: 'opentosca-marketplace',
@@ -11,7 +13,9 @@ import {Subject, Observable} from "rxjs";
 export class MarketplaceComponent implements OnInit {
 
     apps: Application[];
-    categoriesAry = <{category: string, apps: Application[]}[]>[];
+    categoriesAry = <Category[]>[];
+    filteredCategoriesAry = <Category[]>[];
+    private searchTermStream = new Subject<string>();
 
     constructor(private appService: ApplicationService) {
     }
@@ -21,33 +25,51 @@ export class MarketplaceComponent implements OnInit {
         this.getApps();
     }
 
-    search(term: string): {category: string, apps: Application[]}[] {
-        //TODO
-        return
+    search(term: string): void {
+        /*
+         * first of all, we filter locally
+         * 0. bin filtered array to template and keep data from server seperatelly
+         * 1. fetch apps from server
+         * 2. generate categories array
+         * 3. enter search term
+         *
+         * 4. iterate all categories and remove all non-matching apps from arrays
+         * 5. this.filteredCategoriesAry = new categories array
+         *
+         */
     }
 
     getApps(): void {
         this.appService.getApps().then(apps => {
             this.apps = apps;
-            for (let app of apps) {
-                for (let category of app.categories) {
-                    this.addToCategoriesAry(category, app);
-                }
-            }
+            this.categoriesAry = this.generateCategoriesAry(apps);
+            this.filteredCategoriesAry = this.generateCategoriesAry(apps);
         });
     }
 
-    addToCategoriesAry(category: string, app: Application): void {
+    generateCategoriesAry(apps: Application[]): Category[] {
+        let ary = [];
+        for (let app of apps) {
+            for (let category of app.categories) {
+                this.addToCategoriesAry(category, app, ary);
+            }
+        }
+        return ary;
+    }
+
+    addToCategoriesAry(category: string, app: Application, categoriesAry: Category[]): void {
         let found: boolean = false;
-        for (let i in this.categoriesAry) {
-            if (this.categoriesAry[i].category === category) {
-                this.categoriesAry[i].apps.push(app);
+        for (let i in categoriesAry) {
+            if (categoriesAry[i].category === category) {
+                categoriesAry[i].apps.push(app);
                 found = true;
                 break;
             }
         }
         if (!found) {
-            this.categoriesAry.push({category: category, apps: [app]});
+            categoriesAry.push({category: category, apps: [app]});
         }
     }
 }
+
+
---
+++ src/app/shared/category.model.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Michael Falkenthal on 14.09.16.
+ */
+
+import {Application} from "./application.model";
+
+export class Category {
+    public category: string;
+    public apps: Application[];
+}
