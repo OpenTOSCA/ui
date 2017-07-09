3e246d315aa989002fc6a02e7d248d9cb1161eb0
michael.falkenthal@iaas.uni-stuttgart.de <michael.falkenthal@iaas.uni-stuttgart.de>
2017-07-09 21:40:33 +0200

    Add model classes that correspond to entities provided by new api

---
+++ src/app/core/model/new-api/csar-list.model.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     Michael Falkenthal - initial implementation
+ */
+import { CsarListEntry } from './csar-list-entry.model';
+import { ResourceSupport } from './resource-support.model';
+
+export class CsarList extends ResourceSupport{
+    csars: Array<CsarListEntry>;
+}
---
+++ src/app/core/model/new-api/csar.model.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2017 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     Michael Falkenthal - initial implementation
+ */
+import { ResourceSupport } from './resource-support.model';
+
+export class Csar extends ResourceSupport{
+    authors: Array<string>;
+    description: string;
+    display_name: string;
+    icon_url: string;
+    id: string;
+    image_url: string;
+    name: string;
+    version: string;
+}
