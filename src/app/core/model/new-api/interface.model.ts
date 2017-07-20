efc7094333d930250c15c0e6c74d30787dca35f2
michael.falkenthal@iaas.uni-stuttgart.de <michael.falkenthal@iaas.uni-stuttgart.de>
2017-07-20 23:25:23 +0200

    Add interface.model

---
+++ src/app/core/model/new-api/interface.model.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2017 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     Michael Falkenthal - initial implementation
+ */
+import { ResourceSupport } from './resource-support.model';
+import { Operation } from './operation.model';
+
+export class Interface extends ResourceSupport {
+    name: string;
+    operations: Map<string, Operation>;
+}
