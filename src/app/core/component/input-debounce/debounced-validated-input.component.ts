4f6357769fb7df161d30c41b0c8a6f30f82f6807
Tobias Wältken <tobias@waeltken.info>
2017-10-06 17:40:43 +0200

    finish the debounced validated input component

--- src/app/core/component/input-debounce/debounced-validated-input.component.ts
+++ src/app/core/component/input-debounce/debounced-validated-input.component.ts
@@ -9,6 +9,8 @@
  * Contributors:
  *     Tobias Wältken
  */
+import { Http } from '@angular/http';
+import { async } from '@angular/core/testing/src/testing';
 
 import { Component, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
@@ -18,7 +20,7 @@ import { Observable } from 'rxjs/Observable';
     selector: 'opentosca-ui-debounced-validated-input',
     template: `
         <div
-                class="from-group has-feedback"
+                class="form-group has-feedback"
                 [ngClass]="{
                     'has-success': inputValidated && inputValid,
                     'has-danger': inputValidated && !inputValid
@@ -44,30 +46,36 @@ export class DebouncedValidatedInputComponent {
     placeholder: string;
 
     @Input()
-    validator: (value: string) => boolean
+    validator: (value: string) => Observable<boolean>
 
     @Output()
     valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+    @Output()
+    validityChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     inputValue: string;
 
     inputValidated = false;
     inputValid = false;
 
-    constructor(private elementRef: ElementRef) {
-        Observable.fromEvent(elementRef.nativeElement, 'keyup')
-                  .map(() => this.inputValue)
-                  .debounceTime(this.delay)
-                  .distinctUntilChanged()
-                  .subscribe(input => {
-                      if (input) {
-                          this.inputValid = this.validator(input)
-                          this.inputValidated = true;
-                      } else {
-                          this.inputValidated = false;
-                      }
-
-                      this.valueChange.emit(input);
-                  });
+    constructor(private elementRef: ElementRef, private http: Http) {
+        Observable
+            .fromEvent(elementRef.nativeElement, 'keyup')
+            .map(() => this.inputValue)
+            .debounceTime(this.delay)
+            .distinctUntilChanged()
+            .subscribe(input => {
+                if (this.validator && input) {
+                    this.validator(input).subscribe(valid => {
+                        this.validityChange.emit(valid);
+                        this.inputValid = valid;
+                        this.inputValidated = true;
+                    });
+                } else {
+                    this.inputValidated = false;
+                }
+                this.valueChange.emit(input);
+            });
     }
 }
