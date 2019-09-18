5f17338ed1b041bf1b88f000ebfd5f1d85c3576e
Tobias Mathony <mathony.tobias@gmail.com>
2019-09-18 14:34:14 +0200

    added formdata with csarid, servicetemplate name and list of node templates to be placed to call at placement endpoint

--- src/app/core/service/placement.service.ts
+++ src/app/core/service/placement.service.ts
@@ -6,6 +6,8 @@ import { Path } from '../path';
 import { NgRedux } from '@angular-redux/store';
 import { AppState } from '../../store/app-state.model';
 import { Observable } from 'rxjs';
+import { ServiceTemplate } from '../model/service-template.model';
+import { ApplicationManagementService } from './application-management.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +16,12 @@ export class PlacementService {
 
     httpOptions = {
         headers: new HttpHeaders({
-            'Accept': 'application/json'
+            'content-type': 'application/json'
         })
     };
 
-    constructor(private http: HttpClient, private logger: LoggerService, private ngRedux: NgRedux<AppState>) {
+    constructor(private http: HttpClient, private logger: LoggerService, private ngRedux: NgRedux<AppState>,
+                private appService: ApplicationManagementService) {
     }
 
     /**
@@ -26,12 +29,19 @@ export class PlacementService {
      *  that can be used to place the node templates of the node template list
      * @param nodeTemplateList: node templates that need to be placed, i.e. that are of abstract OperatingSystem node type
      */
-    getAvailableInstances(nodeTemplateList: NodeTemplate[]): Observable<any> {
+    getAvailableInstances(csarId: string, nodeTemplateList: NodeTemplate[]): Observable<any> {
+        console.log(this.appService.getFirstServiceTemplateOfCsar(csarId));
         const url = new Path(this.ngRedux.getState().administration.containerUrl)
+            .append('csars')
+            .append(csarId)
+            .append('servicetemplates')
+            .append('%257Bhttp:%252F%252Fopentosca.org%252Fservicetemplates%257DMyTinyToDo_Bare_Docker_AbstractOS')
             .append('placement')
             .toString();
         const formData: FormData = new FormData();
-        formData.append('nodeTemplateList', JSON.stringify(nodeTemplateList));
+        formData.append('csar', csarId);
+        formData.append('servicetemplate', '%257Bhttp:%252F%252Fopentosca.org%252Fservicetemplates%257DMyTinyToDo_Bare_Docker_AbstractOS');
+        formData.append('nodetemplates', JSON.stringify(nodeTemplateList));
         return this.http.post<any>(url, formData, this.httpOptions);
     }
 
