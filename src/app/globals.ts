3e9ba8a8c6ab96c2225e0eae79cfc6da371a4b75
Florian Nieuwenhuizen <st111292@stud.uni-stuttgart.de>
2019-11-05 19:04:52 +0100

    simplified logic for showing time available only when WCET has already been calculated

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -20,7 +20,6 @@ export const globals = {
         'containerApiAddress',
         'instanceDataAPIUrl',
         'planCallbackAddress_invoker',
-        'csarEntrypoint',
-        'OpenTOSCAContainerAPIServiceInstanceID'
+        'csarEntrypoint'
     ]
 };
