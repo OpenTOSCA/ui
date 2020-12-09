74e2f8fc0105b8a986a525fdd268ef13f8440f1c
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2020-12-09 17:27:10 +0100

    removes unnecessary comment blocks adds more space between buttons on application page between buttons
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about.component';
-//
 import { SituationModule } from './situations/situation.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
